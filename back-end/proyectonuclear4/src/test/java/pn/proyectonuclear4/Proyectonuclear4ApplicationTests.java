6ccfab93b98d45e830099f5288df029e235c52e8
magicjuandi <jdgraoorms@gmail.com>
2025-04-15 21:54:32 -0500

    BE

---
+++ back-end/proyectonuclear4/src/test/java/pn/proyectonuclear4/Proyectonuclear4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pn.proyectonuclear4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectonuclear4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
